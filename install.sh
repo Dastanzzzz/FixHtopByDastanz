43aaca27cd5acad9c0881d9d7f4dea91b72fc54c
Dastanzz <destantyo.browen@gmail.com>
2026-07-20 01:56:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,56 +2,14 @@
 
 set -e
 
-echo "======================================"
-echo " Installing htop with sensor support "
-echo "======================================"
+echo "Installing custom htop..."
 
-# Install dependencies
 apt update
 
-apt install -y \
-    git \
-    build-essential \
-    autoconf \
-    automake \
-    pkg-config \
-    libsensors-dev \
-    libncurses-dev
+apt install -y libsensors5
 
+dpkg -i htop-custom_3.6.0-1_arm64.deb
 
-# Download source
-WORKDIR="/tmp/htop-build"
-
-rm -rf "$WORKDIR"
-
-git clone \
-    --depth 1 \
-    https://github.com/htop-dev/htop.git \
-    "$WORKDIR"
-
-
-cd "$WORKDIR"
-
-
-# Generate configure script
-./autogen.sh
-
-
-# Configure with lm-sensors support
-./configure
-
-
-# Build
-make -j"$(nproc)"
-
-
-# Install
-make install
-
-
-echo
-echo "======================================"
-echo " htop installed successfully"
-echo "======================================"
+echo "Done"
 
 htop -V
